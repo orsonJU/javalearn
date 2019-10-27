f523eca5cf5ddd9ab8819cc6afb050a3990da6ba
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2019-10-27 16:34:11 +0800

    pratice insert sort

--- src/main/java/org/orson/algorithm/insert/InsertSort.java
+++ src/main/java/org/orson/algorithm/insert/InsertSort.java
@@ -6,13 +6,8 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] arr = {6, 8, 1, 3, 9, 2};
-
         InsertSort app = new InsertSort();
-
-
         app.sort(arr);
-
-
         for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ", ");
         }
@@ -20,44 +15,21 @@ public class InsertSort {
 
 
     public void sort(int[] arr) {
-        int point = 0;
-        for(int i = 1; i <= arr.length - 1; i++) {
-
-            boolean ischange = false;
-            int temp = arr[i];
-
-            for(int j = i; j > 0 && arr[j-1] > temp; j--) {
-                arr[j] = arr[j-1];
-                ischange = true;
-                point = j;
-            }
-
-            if(ischange) {
-                arr[point - 1] = temp;
-            }
-
-        }
-    }
-
-
-
-
-    public void sort_2(int[] arr) {
-
+        // 变量i表示已经完成排序的元素个数，一开始是0
         for(int i = 0; i < arr.length - 1; i++) {
-
+            // 变量j负责从左往右依次查找需要插入元素的位置
             int j = i + 1;
-            int value = arr[j];
-
-            while(j > 0 && value < arr[j - 1]) {
-                arr[j] = arr[j - 1];
+            // 核心，要保存当前值，然后不断地比较，找到合适的位置
+            int temp = arr[j];
+            while ( j > 0 && temp < arr[j - 1]) {
+                arr[j] = arr[j-1];
                 j--;
             }
-
-            arr[j] = value;
-
+            arr[j] = temp;
         }
+
     }
+
 
     private void swap(int[] arr, int i, int j) {
         int temp = arr[i];
