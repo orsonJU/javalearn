e14dba2fc3592f3834b1eeb114aa8eeeeb05738f
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2019-10-27 16:20:39 +0800

    partice selection sort

--- src/main/java/org/orson/algorithm/selection/SelectionSort.java
+++ src/main/java/org/orson/algorithm/selection/SelectionSort.java
@@ -4,16 +4,9 @@ package org.orson.algorithm.selection;
 public class SelectionSort {
 
     public static void main(String[] args) {
-
         int[] arr = {6, 8, 1, 3, 9, 2};
-
-
         SelectionSort app = new SelectionSort();
-
-
         app.sort(arr);
-
-
         for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ", ");
         }
@@ -21,18 +14,23 @@ public class SelectionSort {
 
 
     public void sort(int[] arr) {
-        int min = 0;
-        for(int i = 0; i < arr.length - 1; i++) {
-            min = i;
-
-            for(int j = arr.length - 1; j > i; j--) {
-                if(arr[j] < arr[min]) {
-                    min = j;
+        // 核心
+        // 变量i表示已经拍好顺序的元素个数，一开始是0
+        for(int i = 0; i < arr.length - 1; i ++) {
+            // 变量j表示从左往右需要进行比较的区域，一开始是0～arr.length - 1
+            int j = 0;
+            // max负责记录最大变量的位置
+            int max = 0;
+            while( j < arr.length - 1 - i) {
+                j++;
+                if(arr[j] > arr[max]) {
+                    max = j;
                 }
             }
-            swap(arr, i, min);
+            swap(arr, max, j);
         }
     }
+
 
 
     public void swap(int[] arr, int i, int j) {
