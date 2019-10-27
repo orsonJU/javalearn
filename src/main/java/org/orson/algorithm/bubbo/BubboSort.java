bfb705f022e1be0f1492974d03e773e8df39ad64
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2019-10-27 16:12:33 +0800

    pratice bubbo sort

--- src/main/java/org/orson/algorithm/bubbo/BubboSort.java
+++ src/main/java/org/orson/algorithm/bubbo/BubboSort.java
@@ -7,33 +7,29 @@ public class BubboSort {
 
     public static void main(String[] args) {
         int[] arr = {6, 8, 1, 3, 9, 2};
-
-
         BubboSort app = new BubboSort();
-
         app.sort(arr);
-
-
         for(int i = 0; i <= arr.length - 1; i++) {
             System.out.print(arr[i] + ", ");
         }
-
-
     }
 
     public void sort(int[] arr) {
-        // 外层的i是指冒泡排序已经拍好的数字个数
-        for(int i = 0; i < arr.length - 1; i++) {
-            // 内层的j是从左往右不断重复执行冒泡
+        // 核心
+        // 变量i表示的是已经通过冒泡排序的元素个数，一开始是0
+        for(int i = 0; i < arr.length - 1; i ++) {
+            // 变量j负责控制从左往右的剩余元素的拍素区间
             for(int j = 0; j < arr.length - 1 - i; j++) {
-                if(arr[j] > arr[j+1]) {
+                if(arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                 }
             }
         }
+
     }
 
-    private void swap(int[] arr, int i, int j) {
+
+    public void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
